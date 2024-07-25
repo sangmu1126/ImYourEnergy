8a491d198047d5492567c20cf86534b723ff5b53
sangmu1126@gmail.com <sangmu1126@gmail.com>
2024-07-26 01:48:07 +0900

    page edit & link

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
-   return 'This is Home!'
+   return render_template('NewJeanSeFindPasswordPage.html')
 
 if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
+   app.run('0.0.0.0', port=5000, debug=True)
